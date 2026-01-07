af0f9b1d39730cc2575912d559dd5dd032296441
MrKalisz <72149056+MrKalisz@users.noreply.github.com>
2026-01-07 10:17:50 -0500

    Update Notes.java

--- Notes.java
+++ Notes.java
@@ -1,6 +1,34 @@
-
 class Notes {
-	public static void main(String[] args) {
+	public static void run() {
+		// By writing // I have made this line a comment
+		// Comments DO NOT execute when the program is run
 
+		//Output - how the computer interacts with us
+
+		// every line of code in java must end in a ;
+		// Coding is case sensetive. 
+		
+		System.out.println("Hi Everybody");
+		//Whatever you want to output goes inside the brackets
+
+		System.out.println(" Hi Everybody");
+
+
+		//spaces outside of "" do nothing
+		//Pressing enter between lines doesn't do anything either
+		//Putting spaces INSIDE "" does change your output
+
+
+		
+		System.out.println        (              "Hi Everybody"  
+  )     ;
+
+		//Numbers do not require quotation marks because they are part of code.
+		System.out.println(10);
+		System.out.println(2.3);
+
+		// system.out.println();  SPELL IT RIGHT AND WITH THE RIGHT CASE SENSETIVITY
+
+		
 	}
 }
