a044f27ac5210005f010a4f5a7df0c5220213ea2
usain-boltsMM <343421046@tdsb.ca>
2026-02-10 16:17:44 +0000

    donei

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -5,19 +5,19 @@ public class PracticeProblem {
 	}
 
 	public static void q1() {
-		//Write question 1 code here
+		System.out.println("Hello World");
 	}
 
 	public static void q2() {
-		//Write question 2 code here
+		System.out.println(" Hello there big fella");
 	}
 
 	public static void q3() {
-		//Write question 3 code here
+		System.out.println(4);
 	}
 
 	public static void q4() {
-		//Write question 4 code here
+		System.out.println(5.3);
 	}
 
 }
